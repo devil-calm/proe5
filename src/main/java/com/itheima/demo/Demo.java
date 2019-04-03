634eedd15f4a0036f1d933a43d074b7840fdc9d0
itheima <1906394343@qq.com>
2019-04-03 19:11:22 +0800

    这是一个测试类

---
+++ src/main/java/com/itheima/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.itheima.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+        System.out.println("Hello,World!");
+        System.out.println("Hello,World!");
+        System.out.println("Hello,World!");
+    }
+}
